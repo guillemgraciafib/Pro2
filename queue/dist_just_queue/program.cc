4b5e05b5332c4a50a0ff6de9698f30548e357f6c
Guillem Gracia Andreu <ggraciadev@gmail.com>
2020-03-20 11:43:11 +0100

    First commit

---
+++ queue/dist_just_queue/program.cc
@@ -0,0 +1,30 @@
+#include "CuaIOParInt.hh"
+#include <vector>
+
+void LlegeixDistribueix(vector<queue<ParInt>>& cua, CuaIOParInt& cuaIO) {
+    vector<int> counter(cua.size(), 0);
+    queue<ParInt> temp;
+    cuaIO.llegirCuaParInt(temp);
+
+    while(!temp.empty()) {
+        int currentQueue = counter[1] > counter[0];
+
+        counter[currentQueue] += temp.front().segon();
+        cua[currentQueue].push(temp.front());
+        temp.pop();
+    }
+}
+
+void EscriuCues(const vector<queue<ParInt>>& cua, CuaIOParInt& cuaIO) {
+    for(int i = 0; i < cua.size(); ++i) {
+        cuaIO.escriureCuaParInt(cua[i]);
+    }
+}
+
+int main() {
+    vector<queue<ParInt>> cua(2);
+    CuaIOParInt cuaIO;
+
+    LlegeixDistribueix(cua, cuaIO);
+    EscriuCues(cua, cuaIO);
+}
