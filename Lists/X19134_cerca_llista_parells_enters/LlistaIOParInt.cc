e96f48a4f4c47c5275de11ccb3bc02fb0d8b4ebe
Guillem Gracia Andreu <ggraciadev@gmail.com>
2020-03-23 12:37:07 +0100

    Exercisi Llista

---
+++ Lists/X19134_cerca_llista_parells_enters/LlistaIOParInt.cc
@@ -0,0 +1,22 @@
+#include "LlistaIOParInt.hh"
+
+
+void LlistaIOParInt::LlegirLlistaParInt(list<ParInt>& llista) {
+    ParInt temp;
+
+    temp.llegir();
+
+    while(temp.primer() != 0 && temp.segon() != 0) {
+        llista.push_back(temp);
+        temp.llegir();
+    }
+}
+
+void LlistaIOParInt::EscriureLlistaParInt(const list<ParInt>& llista) {
+    int size = llista.size();
+    list<ParInt>::const_iterator it;
+
+    for(it = llista.begin(); it != llista.end(); ++it) {
+        it->escriure(); //tambien puede ser (*it).escriure(), pero mejor como yo lo he puesto
+    }
+}
