2cb5e86758bb3dc91face1c70177ee12039a708f
Guillem Gracia Andreu <ggraciadev@gmail.com>
2020-03-27 11:22:22 +0100

    parells enters bin tree created

---
+++ Lists and BinTrees/X20646_CercaParellsEntersBinTree/BinTreeIOEst.cc
@@ -0,0 +1,9 @@
+#include "BinTreeIOEst.hh"
+
+void read_bintree_est(BinTree<ParInt>& a) {
+    
+}
+
+void write_bintree_est(const BinTree<ParInt>& a) {
+    
+}
--- Lists and BinTrees/X65608_CercaEstudiantsBinTree/BinTreeIOEst.cc
+++ Lists and BinTrees/X65608_CercaEstudiantsBinTree/BinTreeIOEst.cc
@@ -1,1 +1,9 @@
 #include "BinTreeIOEst.hh"
+
+void read_bintree_est(BinTree<Estudiant>& a) {
+    
+}
+
+void write_bintree_est(const BinTree<Estudiant>& a) {
+    
+}
