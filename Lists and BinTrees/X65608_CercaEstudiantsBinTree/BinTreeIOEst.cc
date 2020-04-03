f16bf283adfc00ac14c6f51257818ba965e1c3a6
Guillem Gracia Andreu <ggraciadev@gmail.com>
2020-04-03 12:04:48 +0200

    Cerca estudiants bin tree

--- Lists and BinTrees/X65608_CercaEstudiantsBinTree/BinTreeIOEst.cc
+++ Lists and BinTrees/X65608_CercaEstudiantsBinTree/BinTreeIOEst.cc
@@ -1,7 +1,18 @@
 #include "BinTreeIOEst.hh"
 
 void read_bintree_est(BinTree<Estudiant>& a) {
-    
+    Estudiant est;
+    est.llegir();
+
+    if(est.consultar_DNI() != 0) {
+        BinTree<Estudiant> estL;
+        BinTree<Estudiant> estR;
+
+        read_bintree_est(estL);
+        read_bintree_est(estR);
+
+        a = BinTree<Estudiant>(est, estL, estR);
+    }
 }
 
 void write_bintree_est(const BinTree<Estudiant>& a) {
--- Lists and BinTrees/X65608_CercaEstudiantsBinTree/program.cc
+++ Lists and BinTrees/X65608_CercaEstudiantsBinTree/program.cc
@@ -1,7 +1,49 @@
 #include "BinTreeIOEst.hh"
 
-int main() {
-    BinTree<Estudiant>tree;
+Estudiant CercaEstudiant(const BinTree<Estudiant>& tree, int dni, int& profunditat) {
+	Estudiant est;
 
-    
+	if (tree.empty()) {
+		profunditat = -1;
+	}
+	else if (tree.value().consultar_DNI() == dni) {
+		est = tree.value();
+		profunditat = 0;
+	}
+	else {
+		est = CercaEstudiant(tree.left(), dni, profunditat);
+		if (profunditat == -1) {
+			est = CercaEstudiant(tree.right(), dni, profunditat);
+		}
+		if (profunditat != -1) {
+			profunditat++;
+		}
+	}
+
+	return est;
+}
+
+int main() {
+	BinTree<Estudiant>tree;
+	read_bintree_est(tree);
+
+	int current;
+	while (cin >> current) {
+		int profunditat = 0;
+		Estudiant temp = CercaEstudiant(tree, current, profunditat);
+
+		cout << current << " " << profunditat;
+		if (profunditat != -1) {
+			cout << " ";
+			if (temp.te_nota()) {
+				cout << temp.consultar_nota() << endl;
+			}
+			else {
+				cout << -1 << endl;
+			}
+		}
+		else {
+			cout << endl;
+		}
+	}
 }
